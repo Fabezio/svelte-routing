68b47f7a9b26aab1be72caa7ee22651868ef4476
fabezio <fabezio@outlook.fr>
2020-07-25 23:00:00 +0200

    init

---
+++ src/pages/blog/[slug].svelte
@@ -0,0 +1,9 @@
+<script>
+  export let slug
+  $: console.log($$props)
+  // $: console.log(getCurrentPosition)
+</script>
+
+<h1>
+{slug}</h1>
+
---
+++ src/pages/blog/index.svelte
@@ -0,0 +1,12 @@
+<script>
+  import {url} from '@sveltech/routify'
+  // export let path
+  $: console.log(url)
+</script>
+
+<h1>Blog page</h1>
+
+<a href={$url('blog/machin')}>machin</a><br>
+<a href={$url('blog/truc')}>truc</a><br>
+<a href={$url('blog/bidule')}>bidule</a>
+
